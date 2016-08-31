798aef4a62f4198dc9109a1d1ee117d4f4e2287b
Jesse Laukkanen <jesse.laukkanen@anders.fi>
2016-08-31 14:35:21 +0300

    Initial carousel slider implementation
    Simplistic approach for defining a Carousel model with n related slides.

---
+++ cmsplugin_carousel_ai/cms_plugins.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+
+from .models import Carousel, Slide
+
+
+class CarouselSlideAdmin(admin.StackedInline):
+    model = Slide
+    extra = 1
+
+
+class CarouselPlugin(CMSPluginBase):
+    model = Carousel
+    module = _("Carousel")
+    name = _("Image Carousel")
+    inlines = (CarouselSlideAdmin,)
+    render_template = "cmsplugin_carousel_ai/carousel.html"
+
+    def render(self, context, instance, placeholder):
+        context["carousel"] = instance
+        context["slides"] = instance.slides.all().select_related("linked_page")
+        return context
+
+
+plugin_pool.register_plugin(CarouselPlugin)
