d181508f3c10a7573af2fb5f3ba7b603df174091
Jesse Laukkanen <jesse.laukkanen@anders.fi>
2016-09-01 10:16:14 +0300

    Bump package version to 0.0.1 in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='cmsplugin-carousel-ai',
-    version='0.0.0',
+    version='0.0.1',
     author='Anders Innovations',
     author_email='info@anders.fi',
     packages=['cmsplugin_carousel_ai', 'cmsplugin_carousel_ai.migrations', 'cmsplugin_carousel_ai.templates'],
