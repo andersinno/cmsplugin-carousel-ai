fe24e688dc578479252ab2b583c2489c17596603
Juha Kujala <juha.kujala@anders.fi>
2017-10-05 09:05:47 +0300

    Bump version to 0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='cmsplugin-carousel-ai',
-    version='0.0.2',
+    version='0.0.3',
     author='Anders Innovations',
     author_email='info@anders.fi',
     packages=['cmsplugin_carousel_ai', 'cmsplugin_carousel_ai.migrations', 'cmsplugin_carousel_ai.templates'],
